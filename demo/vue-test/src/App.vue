f421e051fa3e2826d6871bf3fde9372e85308977
薛遥智 <1113213748@qq.com>
2019-03-30 14:05:14 +0800

    underscore template

---
+++ demo/vue-test/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'App',
+    mounted(){
+      debugger
+      axios.post();
+      console.log(axios);
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
