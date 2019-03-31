f92e373ba6aab8157915a50d43ff580dff069d6e
薛遥智 <1113213748@qq.com>
2019-03-31 21:32:57 +0800

    新增axios 源码解读部分

--- demo/node-mock/server/server.js
+++ demo/node-mock/server/server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 var cors = require('cors');//它可以响应前端发送过来的 API 请求，并且让浏览器接受响应信息。
 
 const test = require('./routers/mockTest');
+const axios = require('./routers/aoxisTest');
 
 app.use(bodyParser.json());
 // 如果想使用form提交，这一行是必要的
@@ -21,3 +22,4 @@ app.listen(9093, function () {
     console.log('Node app start at port 9093')
 })
 test(app);
+axios(app);
